b40dcf108bb681bcfe20c3eeb3ed69ef8a7d8eaf
ArTh3rGeOrgE <109643926+ArTh3rGeOrgE@users.noreply.github.com>
2022-09-14 06:38:18 +0530

    Updated_code

--- Main.py
+++ Main.py
@@ -3,3 +3,13 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 # Write your code here
+Original_List = []
+count = 0
+while len(Original_list) < length_of_circular_linked_list and count < len(circular_linked_list)
+  ele = circular_linked_list[count]
+  if ele not in Orignal_list:
+    Original_list.append(ele)
+   count +=1
+ 
+print(len(Orignal_list)
+print(" ".join(str(num) for num in Original_list))
