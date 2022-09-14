2e4e0a9485699ff5565fb865fd5e186471485b3b
ArTh3rGeOrgE <109643926+ArTh3rGeOrgE@users.noreply.github.com>
2022-09-14 06:41:32 +0530

    UPDATED

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 # Write your code here
-Original_List = []
+Original_list = []
 
 count = 0
 while len(Original_list) < length_of_circular_linked_list and count < len(circular_linked_list):
